ad31797da4285d32af930554e51c002d5fcd4c3d
SVEN <1010773095@qq.com>
2017-07-06 01:02:04 +0800

    first commit

---
+++ app/src/main/java/com/example/sven/broadcastclient/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sven.broadcastclient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendBroadcastNeedPermission(View view){
+        sendBroadcast(new Intent("com.sven.action.my.receiver.need.permission"));
+    }
+
+    public void sendBroadcastWithPermission(View view){
+        sendBroadcast(new Intent("com.sven.action.my.receiver.with.permission"), "com.sven.permission.my.receiver.RECEIVE");
+    }
+
+    public void sendBroadcastWithFlag(View view){
+        Intent intent = new Intent("android.intent.action.myreceiver");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+    }
+
+}
